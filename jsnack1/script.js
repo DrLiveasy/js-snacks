a6f7c058ad00640c05929d92c402e1b7a26b909d
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-14 16:40:24 +0100

    non avevo idetificato che num sono numeri

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,23 +1,18 @@
 // input prompt 
-let num1 = prompt("Inserisci un numero!");
-let num2 = prompt("Inserisci un'altro numero !");
-
-// output del numero maggiore 
-
-// prima idetificare se è stato inserito un numero
-if (isNaN(num1) || isNaN (num2)){
-    console.log("Perfavore inserisci un numero !!");
+let num1 = parseInt (prompt("Inserisci un numero!"));
+let num2 = parseInt (prompt("Inserisci un'altro numero !"));
+ 
 
 // i due numeri sono uguali o no ?
-}else if ( num1 == num2 ){
+if ( num1 == num2 ){
     console.log("i due numeri sono uguali");
 
 // se i due numero non sono uguali stampa il numero maggiore
 }else{
-    if(num1>num2){
+    if(num1 > num2){
         console.log("il numero più grande è : "+ num1);
 
     }else{
-        console.log("il numero èiù grande è : "+num2);
+        console.log("il numero èiù grande è : "+ num2);
     }
 }
