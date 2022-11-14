9c0eb78f5b8d8049bb9ea0ff341d5b0888b60d77
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-14 17:40:33 +0100

    prompt + generare numero casuale

---
+++ jsnack6/script.js
@@ -0,0 +1,9 @@
+
+let numeroUtente = prompt("inserire un numero da 0 a 10");
+
+//faccio generare un numero casuale da 0 a 10 
+
+let numeroRandom = Math.random() * 11;
+let numeroRandomIntero = Math.floor(numeroRandom);
+
+
