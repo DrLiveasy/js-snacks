d2b4b8123a571db81a0f453af1e8a6bec1522466
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-14 14:41:06 +0100

    finito l'esercizio stampando il numero maggiore

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,9 +4,20 @@ let num2 = prompt("Inserisci un'altro numero !");
 
 // output del numero maggiore 
 
+// prima idetificare se è stato inserito un numero
 if (isNaN(num1) || isNaN (num2)){
     console.log("Perfavore inserisci un numero !!");
 
+// i due numeri sono uguali o no ?
 }else if ( num1 == num2 ){
     console.log("i due numeri sono uguali");
+
+// se i due numero non sono uguali stampa il numero maggiore
+}else{
+    if(num1>num2){
+        console.log("il numero più grande è : "+ num1);
+
+    }else{
+        console.log("il numero èiù grande è : "+num2);
+    }
 }
