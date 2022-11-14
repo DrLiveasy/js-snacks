9a02c8b34fec4ec9396fa0e8a86be12983a51ff7
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-14 14:29:01 +0100

    Bonus identificare se è stato inserito un numero

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,10 @@
 // input prompt 
 let num1 = prompt("Inserisci un numero!");
 let num2 = prompt("Inserisci un'altro numero !");
+
+// output del numero maggiore 
+
+if (isNaN(num1) || isNaN (num2)){
+    console.log("Perfavore inserisci un numero !!")
+}
+
