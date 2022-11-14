959794e3c32c24279c047bd69ba37f16ec694b5b
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-14 15:41:48 +0100

    finito il seconde esercizio

---
+++ jsnack2/script.js
@@ -0,0 +1,11 @@
+let parola1 = prompt("inserisci una parola");
+let parola2 = prompt("inserisci un'altra parola!");
+
+ 
+if (parola1 > parola2){
+    console.log("la prola più corta è " + parola2);
+    console.log("la parola piuù grande è "+ parola1);
+}else if(parola2 > parola1){
+    console.log("la prola più corta è " + parola1);
+    console.log("la parola piuù grande è "+ parola2);
+}
