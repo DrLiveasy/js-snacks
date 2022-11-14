3283dc69df29584d38bcf24db9653d42f5a399b3
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-14 17:34:20 +0100

    stampara l'array con i numeri dispari !

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -5,5 +5,12 @@ let dispari = 0;
 for (let i = 0; i < 6; i++) {
     let n = parseInt(prompt("inserisci un numero!"));
 
-}   
+    if (n % 2 == 0) {
+        pari++;
+    } else {
+        arrayNumeriDispari += n + (",") ;
+    }
+}
+
+console.log(arrayNumeriDispari);
 
