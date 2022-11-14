46a255397a4d1912153cb3f654ce7b4aebdeeeca
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-14 17:44:16 +0100

    Finito l'esercizio , Stampare il risultato

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -6,4 +6,9 @@ let numeroUtente = prompt("inserire un numero da 0 a 10");
 let numeroRandom = Math.random() * 11;
 let numeroRandomIntero = Math.floor(numeroRandom);
 
-
+// identificare se il numero scelto ugualo o no , poi stampare il risultato !
+if(numeroRandomIntero == numeroUtente){
+    console.log("hai vinto !");
+} else {
+    console.log("hai perso!")
+}
