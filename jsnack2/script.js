76a9fa936b8dc3bf547468d6c9409c0b7e79f782
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-15 09:35:09 +0100

    corregiamento errori es.2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,11 +1,13 @@
 let parola1 = prompt("inserisci una parola");
 let parola2 = prompt("inserisci un'altra parola!");
 
- 
-if (parola1 > parola2){
+
+if (parola1.length > parola2.length){
     console.log("la prola più corta è " + parola2);
-    console.log("la parola piuù grande è "+ parola1);
-}else if(parola2 > parola1){
+    console.log("la parola più grande è "+ parola1);
+}else if(parola2.length > parola1.length){
     console.log("la prola più corta è " + parola1);
-    console.log("la parola piuù grande è "+ parola2);
+    console.log("la parola più grande è "+ parola2);
+}else{
+    console.log("le 2 parole sono alla stessa lunghezza ! ");
 }
