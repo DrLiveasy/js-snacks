a781bc0c25cca43be858a8c2e5f7436ba73f05ae
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-15 09:22:14 +0100

    un correggiamento

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,18 +1,16 @@
 // input prompt 
 let num1 = parseInt (prompt("Inserisci un numero!"));
 let num2 = parseInt (prompt("Inserisci un'altro numero !"));
- 
 
 // i due numeri sono uguali o no ?
 if ( num1 == num2 ){
     console.log("i due numeri sono uguali");
 
 // se i due numero non sono uguali stampa il numero maggiore
-}else{
-    if(num1 > num2){
-        console.log("il numero più grande è : "+ num1);
-
-    }else{
-        console.log("il numero èiù grande è : "+ num2);
-    }
+}else if(num1 > num2){
+    console.log("il numero più grande è : "+ num1);
 }
+else{
+    console.log("il numero èiù grande è : "+ num2);
+}
+
