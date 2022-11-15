58400959e2251010d89ab2bb8461df8929c7cdbb
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-15 09:47:33 +0100

    corregiamenti

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,9 +1,15 @@
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Javascript - JS
+*/
+
 let n, somma = 0;
 
 for (let i = 0; i < 10; i++) {
-  n = parseInt(prompt('Inserisci un numero'));
+  n = (parseInt(prompt('Inserisci un numero')));
   somma += n;
 }
 
 
-console.log('la somma è ', somma)
+console.log('la somma è ', somma);
